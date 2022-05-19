d06adc8fac9281b314a6d427262414e5009e2b89
Max Bernstein <max@bernsteinbear.com>
2022-05-19 01:04:41 -0700

    Bail early on allocation failure

--- fmt.h
+++ fmt.h
@@ -11,6 +11,7 @@ typedef struct str_size {
   size_t length;
 } str_size;
 
+// Returns NULL str if allocation or formatting fails.
 static str_size formatv(const char *fmt, va_list args) {
   va_list args_copy;
   va_copy(args_copy, args);
@@ -22,11 +23,15 @@ static str_size formatv(const char *fmt, va_list args) {
   result.length = (size_t)size;
   // +1 to include NUL
   result.str = malloc(size + 1);
+  if (result.str == NULL) {
+    return result;
+  }
   vsnprintf(result.str, size + 1, fmt, args_copy);
   va_end(args_copy);
   return result;
 }
 
+// Returns NULL str if allocation or formatting fails.
 __attribute__((format(printf, 1, 2))) static str_size format(const char *fmt,
                                                              ...) {
   va_list args;
